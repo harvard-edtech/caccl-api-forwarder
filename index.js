af4f01d2fb4e089cd1c2dd747306f2cceaa88048
Gabe Abrams <gabeabrams@gmail.com>
2020-02-04 15:54:54 -0500

    Simplified request forwarder

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ module.exports = (config) => {
   const numRetries = (config.numRetries !== undefined ? config.numRetries : 3);
 
   // Add route
-  config.app.all(`${apiForwardPathPrefix}*`, (req, res) => {
+  config.app.all(`${apiForwardPathPrefix}*`, async (req, res) => {
     const isGET = (req.method === 'GET');
     const data = (isGET ? req.query : req.body);
 
@@ -41,86 +41,35 @@ module.exports = (config) => {
       data.access_token = req.accessToken || config.accessToken;
     }
 
-    /**
-     * Attempt to send the request to Canvas
-     * @author Gabe Abrams
-     * @param {boolean} [alreadyRefreshed] - if true, we have already tried to
-     *   refresh the user's authorization before, so don't try that again
-     */
-    const attemptRequest = async (alreadyRefreshed) => {
-      try {
-        // Send the request
-        const response = await sendRequest({
-          host,
-          path,
-          numRetries,
-          method: req.method,
-          params: data,
-          // Ignore self-signed certificate if host is simulated Canvas
-          ignoreSSLIssues: (host === 'localhost:8088'),
-        });
+    // Attempt to send the request to Canvas
 
-        /**
-         * Forward the response to the client
-         * @author Gabe Abrams
-         */
-        const forward = () => {
-          // Set status
-          res.status(response.status);
+    try {
+      // Send the request
+      const response = await sendRequest({
+        host,
+        path,
+        numRetries,
+        method: req.method,
+        params: data,
+        // Ignore self-signed certificate if host is simulated Canvas
+        ignoreSSLIssues: (host === 'localhost:8088'),
+      });
 
-          // Send link header
-          res.header('Access-Control-Expose-Headers', 'Link');
-          res.set('Link', response.headers.link);
+      // Set status
+      res.status(response.status);
 
-          // Send request
-          return res.json(response.body);
-        };
+      // Send link header
+      res.header('Access-Control-Expose-Headers', 'Link');
+      res.set('Link', response.headers.link);
 
-        // Detect invalid access token message
-        if (
-          response.status === 401
-          && !alreadyRefreshed
-          && response.body
-          && response.body.errors
-          && response.body.errors[0]
-          && response.body.errors[0].message
-          && response.body.errors[0].message === 'Invalid access token.'
-        ) {
-          // Invalid token!
-          // Attempt to refresh
-          let results;
-          try {
-            results = await req.performRefresh();
-          } catch (err) {
-            results = null;
-          }
-
-          // If successful, add the token and retry
-          if (results && results.accessToken) {
-            // Add the new access token to the body
-            const { accessToken } = results;
-            data.access_token = accessToken;
-            // Retry the request
-            return attemptRequest(true);
-          }
-
-          // An error occurred while attempting to refresh.
-          // Just forward the original failure response
-          return forward();
-        }
-
-        // Valid token. Forward response immediately
-        return forward();
-      } catch (err) {
-        return (
-          res
-            .status(500)
-            .send(`We encountered an error while attempting to contact Canvas and forward an API request: ${err.message}`)
-        );
-      }
-    };
-
-    // Immediately attempt the request
-    attemptRequest();
+      // Send request
+      return res.json(response.body);
+    } catch (err) {
+      return (
+        res
+          .status(500)
+          .send(`We encountered an error while attempting to contact Canvas and forward an API request: ${err.message}`)
+      );
+    }
   });
 };
