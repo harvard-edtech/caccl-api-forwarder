77e01e5f33adc3b70890e4f4041dc4d4e26a9b79
Gabe Abrams <gabeabrams@gmail.com>
2020-02-04 16:06:49 -0500

    Added console log for debugging

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ module.exports = (config) => {
     if (!data.access_token) {
       data.access_token = req.accessToken || config.accessToken;
     }
+    console.log('Forwarding', path, data);
 
     // Attempt to send the request to Canvas
 
