cef47f6661694ba1e8eeeb7c96d48bd77c6162c7
Gabe Abrams <gabeabrams@gmail.com>
2022-02-23 12:25:10 -0500

    Added main export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,12 @@
-export {};
+import express from 'express';
+/**
+ * Initializes api forwarding
+ * @author Gabe Abrams
+ * @param {express.Application} app the express app to add routes to
+ * @param {number} [numRetries=3] the number of times to retry failed requests
+ */
+declare const initAPIForwarder: (opts: {
+    app: express.Application;
+    numRetries?: number;
+}) => void;
+export default initAPIForwarder;
